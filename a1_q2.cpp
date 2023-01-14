53f5ea0176ae9906d5aba94a52a3e67d1749897a
Akshita-Goel <122682368+Akshita-Goel@users.noreply.github.com>
2023-01-14 20:56:58 +0530

    Submitting Q2 of Assignment 1

---
+++ a1_q2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main{
+    int marks;
+    cin>>marks;
+    
+    if(marks<0 || marks>100)
+    {
+        cout<<"invalid";
+    }
+    if(marks>90)
+    {
+        cout<<"excellent"<<endl;
+        
+    }
+    else if(marks>80 && marks<=90)
+    {
+        cout<<"good";
+    }
+    else if(marks>70 && marks<=80)
+    {
+        cout<<"fair";
+    }
+    else if(marks>60 && marks<=70)
+    {
+        cout<<"meets expectations";
+    }
+    else if(marks<60)
+    {
+        cout<<"below par";
+    }
+    return 0;
+}
