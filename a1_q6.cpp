75186de6aea20ff42d10d6c21c85af44fc2b41f1
Akshita-Goel <122682368+Akshita-Goel@users.noreply.github.com>
2023-01-15 00:18:29 +0530

    Submitting Q6 of Assignment 1

---
+++ a1_q6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter a no.: ";
+    cin>>num;
+    int count =0;
+    while(num != 0){
+        num=num/10;
+        count=count+1;
+    }
+    cout<<count;
+    return 0;
+}
