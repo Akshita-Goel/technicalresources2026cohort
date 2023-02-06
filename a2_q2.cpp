ae6d6305cb6f15000fda967e76fb65f842947f58
Akshita-Goel <122682368+Akshita-Goel@users.noreply.github.com>
2023-02-06 14:45:37 +0530

    Submitting Q2 of Assignment 2

---
+++ a2_q2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n,a=0,b=1;
+    cin>>n;
+    cout<<a<<endl;
+    cout<<b<<" "<<b<<endl;
+    a=1;
+    for(int row=3; row<=n; row++)
+    {
+        for(int col=1;col<=row;col++)
+        {
+            int c=a+b;
+            cout<<c<<" ";
+            a=b;
+            b=c;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+      
